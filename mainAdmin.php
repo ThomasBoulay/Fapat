c8e621a477a8edb9ef5e11e7a92c9e6eb6bf265e
thomas <thomasboulay92@gmail.com>
2019-12-06 15:38:06 +0100

    page admin : ajout des trois menu deroulants (sans position)

--- mainAdmin.php
+++ mainAdmin.php
@@ -25,17 +25,30 @@ session_start();
             </div>
         </div>
         
-        <div class="corps">
-            
-            <div class="logo1" style=" background-image: url('images/internet.png');">
-                <div class="dropdown-content-logo" style="right: 0">
-                    <a href="main.php">Modifer</a>
-                    <a href="mainEn.php">Consulter</a>
-                </div>
-            </div>
-            
-        </div>
-       
+      <div class="dropdown">
+  <img src="images/logoTest.png" width="50" height="50">
+  <div class="dropdown-content">
+  <a href="#">Modifier</a>
+  <a href="#">Consulter</a>
+  </div>
+</div>
+    
+    <div class="dropdown">
+  <img src="images/logoInfo.png" width="50" height="50">
+  <div class="dropdown-content">
+  <a href="#">Documentation</a>
+  <a href="#">Statistiques</a>
+  </div>
+</div>
+    
+    <div class="dropdown">
+  <img src="images/logoProfil.png" width="50" height="50">
+  <div class="dropdown-content">
+  <a href="#">Ajouter un utilisateur</a>
+  <a href="#">Modifier un profil</a>
+  </div>
+</div>
+    
         <?php
         include ('piedPage.php');
         ?>
