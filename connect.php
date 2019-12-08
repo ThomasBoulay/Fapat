23c554f6da51c66e961d179a9a95b901ccc4ad8f
Lim-Sean <56913276+Lim-Sean@users.noreply.github.com>
2019-12-08 20:19:37 +0100

    Nouvelle Page de Connexion

--- connect.php
+++ connect.php
@@ -18,33 +18,33 @@
         <?php
         include ('enTete.php');
         ?>
+        <div class="background">
 
-        <div id="login" style="width: 50%; display: block; margin-left: auto; margin-right: auto;">
-            <form action="logIn.php" method="post" class="login">
-                <div style="padding: 15px">
-                    <label>
-                        <b>Pseudo</b>
-                    </label>
-                    <input id="name" type="text" placeholder="Username" name="username" required>
-                    
-                    <label>
-                        <b>Mot de Passe</b>
-                    </label>
-                    <input id="password" type="password" placeholder="Password" name="password" required>
-                    
-                    <button name="submit" type="submit" class="connect">
-                        Se connecter
-                    </button>
+            <form action="logIn.php" method="post" class="loginBox">
+              <h1>Connectez-vous</h1>
+              <div class="loginWrite">
+                  <input id ="name" type="text" name="name" required>
+                  <label for="name" class="label-name">
+                      <span class="content-name">Identifiant</span>
+                  </label>
+              </div>
+
+              <div class="loginWrite">
+                  <input id = password type="password" name="password" required>
+                  <label for="password" class="label-name">
+                      <span class="content-name">Mot de Passe</span>
+                  </label>
+              </div>
+
+              <button name="submit" type="submit" class="logButton" value ="Se Connecter">Se connecter</button>
                     <?php
                     if (isset($_SESSION['failed']) && $_SESSION['failed']==1){
                     echo("<label><b>Veuillez réessayer</b></label>");
                     $_SESSION['failed']=0;
                     }
                     ?>
-                </div>
             </form>
         </div>
-
         <?php
         include ('piedPage.php');
         ?>
